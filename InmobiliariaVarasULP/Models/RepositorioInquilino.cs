47b6798c0f137e016f8fa94b2f8150c5e1f44bec
PamV83 <pamvaras83@gmail.com>
2021-04-04 22:47:35 -0300

    Agregar archivos de proyecto.

---
+++ InmobiliariaVarasULP/Models/RepositorioInquilino.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaVarasULP.Models
+{
+   public class RepositorioInquilino : RepositorioBase
+		{
+			public RepositorioInquilino(IConfiguration configuration) : base(configuration)
+			{
+
+			}
+
+			public IList<Inquilino> ObtenerTodos()
+			{
+				IList<Inquilino> res = new List<Inquilino>();
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					string sql = $"SELECT Id, Dni, Nombre, Apellido, DomicilioLaboral, Email, TelefonoInquilino, NombreGarante, DniGarante, TelefonoGarante" +
+						$" FROM Inquilino";
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						command.CommandType = CommandType.Text;
+						connection.Open();
+						var reader = command.ExecuteReader();
+						while (reader.Read())
+						{
+							Inquilino i = new Inquilino
+							{
+								Id = reader.GetInt32(0),
+								Dni = reader.GetString(1),
+								Nombre = reader.GetString(2),
+								Apellido = reader.GetString(3),
+								DomicilioLaboral = reader.GetString(4),
+								Email = reader.GetString(5),
+								TelefonoInquilino = reader.GetString(6),
+								NombreGarante = reader.GetString(7),
+								DniGarante = reader.GetString(8),
+								TelefonoGarante = reader.GetString(9),
+
+							};
+							res.Add(i);
+						}
+						connection.Close();
+					}
+				}
+				return res;
+			}
+			public int Alta(Inquilino i)
+			{
+				int res = -1;
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					string sql = $"INSERT INTO Inquilino ( Dni, Nombre, Apellido, DomicilioLaboral, Email, TelefonoInquilino, NombreGarante, DniGarante, TelefonoGarante) " +
+					$"VALUES ('{i.Dni}', '{i.Nombre}','{i.Apellido}', '{i.DomicilioLaboral}', '{i.Email}', '{i.TelefonoInquilino}', '{i.NombreGarante}', '{i.DniGarante}', '{i.TelefonoGarante}')";
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						command.CommandType = System.Data.CommandType.Text;
+						connection.Open();
+						res = command.ExecuteNonQuery();
+						command.CommandText = "SELECT SCOPE_IDENTITY()";
+						var id = command.ExecuteScalar();
+						i.Id = Convert.ToInt32(id);
+						connection.Close();
+					}
+				}
+				return res;
+			}
+		public Inquilino ObtenerPorId(int id)
+		{
+
+			Inquilino i = null;
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"SELECT Id, Dni, Nombre, Apellido, DomicilioLaboral, Email, TelefonoInquilino, NombreGarante, DniGarante, TelefonoGarante FROM Inquilino" +
+					$" WHERE Id=@id;";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.Parameters.AddWithValue("@id", id);
+					connection.Open();
+					SqlDataReader reader = command.ExecuteReader();
+					if (reader.Read())
+					{
+						i= new Inquilino
+						{
+							Id = reader.GetInt32(0),
+							Dni = reader.GetString(1),
+							Nombre = reader.GetString(2),
+							Apellido = reader.GetString(3),
+							DomicilioLaboral = reader.GetString(4),
+							Email = reader.GetString(5),
+							TelefonoInquilino = reader.GetString(6),
+							NombreGarante = reader.GetString(7),
+							DniGarante = reader.GetString(8),
+							TelefonoGarante = reader.GetString(9),
+						};
+					}
+					connection.Close();
+				}
+				return i;
+			}
+		
+		}
+
+		public int Baja(int id)
+		{
+			int res = -1;
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"DELETE FROM Inquilino WHERE {nameof(Inquilino.Id)} = @id;";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.Parameters.AddWithValue("@id", id);
+					connection.Open();
+					res = command.ExecuteNonQuery();
+					connection.Close();
+				}
+
+			}
+			return res;
+		}
+		public int Modificacion(Inquilino i)
+		{
+			int res = -1;
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"UPDATE Inquilino SET Dni='{i.Dni}',Nombre='{i.Nombre}', Apellido='{i.Apellido}', DomicilioLaboral='{i.DomicilioLaboral}', Email='{i.Email}',, TelefonoInquilino='{i.TelefonoInquilino}', NombreGarante='{i.NombreGarante}',DniGarante='{i.DniGarante}', TelefonoGarante='{i.TelefonoGarante}' " +
+					$"WHERE Id = {i.Id}";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					res = command.ExecuteNonQuery();
+					connection.Close();
+				}
+			}
+			return res;
+		}
+	}
+}
---
+++ InmobiliariaVarasULP/Models/RepositorioPropietario.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaVarasULP.Models
+{
+	public class RepositorioPropietario : RepositorioBase
+	{
+		public RepositorioPropietario(IConfiguration configuration) : base(configuration)
+		{
+
+		}
+
+		public IList<Propietario> ObtenerTodos()
+		{
+			IList<Propietario> res = new List<Propietario>();
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"SELECT Id, Nombre, Apellido, Dni, Telefono,Email, Clave" +
+					$" FROM Propietario";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					var reader = command.ExecuteReader();
+					while (reader.Read())
+					{
+						Propietario p = new Propietario
+						{
+							Id = reader.GetInt32(0),
+							Nombre = reader.GetString(1),
+							Apellido = reader.GetString(2),
+							Dni = reader.GetString(3),
+							Telefono = reader.GetString(4),
+							Email = reader.GetString(5),
+							Clave = reader.GetString(6)
+
+						};
+						res.Add(p);
+					}
+					connection.Close();
+				}
+			}
+			return res;
+		}
+		public int Alta(Propietario p)
+		{
+			int res = -1;
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"INSERT INTO Propietario (Nombre, Apellido, Dni, Telefono, Email, Clave) " +
+				$"VALUES ('{p.Nombre}', '{p.Apellido}', '{p.Dni}', '{p.Telefono}', '{p.Email}', '{p.Clave}')";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.CommandType = System.Data.CommandType.Text;
+					connection.Open();
+					res = command.ExecuteNonQuery();
+					command.CommandText = "SELECT SCOPE_IDENTITY()";
+					var id = command.ExecuteScalar();
+					p.Id = Convert.ToInt32(id);
+					connection.Close();
+				}
+			}
+			return res;
+		}
+		public Propietario ObtenerPorId(int id)
+		{
+
+			Propietario p = null;
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"SELECT Id, Nombre, Apellido, Dni, Telefono, Email, Clave FROM Propietario" +
+					$" WHERE Id=@id;";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.Parameters.AddWithValue("@id", id);
+					connection.Open();
+					SqlDataReader reader = command.ExecuteReader();
+					if (reader.Read())
+					{
+						p = new Propietario
+						{
+							Id = reader.GetInt32(0),
+							Nombre = reader.GetString(1),
+							Apellido = reader.GetString(2),
+							Dni = reader.GetString(3),
+							Telefono = reader.GetString(4),
+							Email = reader.GetString(5),
+							Clave = reader.GetString(6),
+						};
+					}
+					connection.Close();
+				}
+			}
+			return p;
+		}
+		public int Baja(int id)
+		{
+			int res = -1;
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"DELETE FROM Propietario WHERE {nameof(Propietario.Id)} = @id;";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.Parameters.AddWithValue("@id", id);
+					connection.Open();
+					res = command.ExecuteNonQuery();
+					connection.Close();
+				}
+
+			}
+			return res;
+		}
+		public int Modificacion(Propietario p)
+		{
+			int res = -1;
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string sql = $"UPDATE Propietario SET Nombre='{p.Nombre}', Apellido='{p.Apellido}', Dni='{p.Dni}', Telefono='{p.Telefono}', Email='{p.Email}' " +
+					$"WHERE Id = {p.Id}";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					res = command.ExecuteNonQuery();
+					connection.Close();
+				}
+			}
+			return res;
+		}
+	}
+}
